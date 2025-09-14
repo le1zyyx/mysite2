67fd50b920b6bb42bc705966a7913fda1cf22c62
le1zyyx <le1zyyx@gmail.com>
2025-09-14 19:38:03 +0300

    Add photo Kyiv, update layout and styles.

--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def places_list(request):
     return render(request, 'main/places_list.html')
+
+def new_place(request):
+    return render(request, 'main/new_place.html')
+
+def random_place(request):
+    return render(request, 'main/random_place.html')
